9e0501f13ec047934b0aecf51ccc7ed519d55f4f
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-06-14 12:42:24 +0530

    added authentication to email and password

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,11 @@
 const express=require('express')
 
 import router from "./routes/auth";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app=express()
+
+dotenv.config()
 
 app.use(express.json())
 
--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,8 +1,16 @@
 import express from 'express'
+import { body, validationResult } from 'express-validator';
 
 const router=express.Router();
 
-router.post('/login',(req,res,next)=>{
+router.post('/login',
+body('email').isEmail().withMessage('Email is invalid'),
+body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters long')
+,(req,res,next)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.json({errors:errors.array().map(err=>err.msg)})
+    }
  const   {email,password}=req.body;
     res.json({
         email,

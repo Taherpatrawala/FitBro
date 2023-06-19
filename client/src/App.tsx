b4093cd5ab3870a9c6b39a04396b97863dc52578
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-06-19 13:12:42 +0530

    Started working on the signin/login route/frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,22 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Login from "./components/login/login"
-import Navbar from "./components/navbar"
-import Body from "./components/body/body"
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login/login";
+import Navbar from "./components/navbar";
+import Body from "./components/body/body";
 
 function App() {
-  
-
   return (
     <BrowserRouter>
-    <div>
-    <Navbar/>
-      <Routes>
-          
-          
-          <Route path="/" element={<Body/>}/>
-         
-      
-          <Route path='/login' element={<Login/>}/>
-      </Routes>
-      
-    </div>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Body />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<Login />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/body/body.tsx
+++ client/src/components/body/body.tsx
@@ -1,9 +1,34 @@
-
+import { NavLink } from "react-router-dom";
 
 const Body = () => {
   return (
-    <div className="h-screen w-screen bg-cover z-10" style={{backgroundImage:'url(/src/assets/clay-banks-w_qTfiPbjbg-unsplash.jpg)'}}>
-      <button>Sign Up</button>
+    <div
+      className="h-screen w-screen bg-cover z-10"
+      style={{
+        backgroundImage: "url(/src/assets/clay-banks-w_qTfiPbjbg-unsplash.jpg)",
+      }}
+    >
+      <div className="h-full w-full flex justify-center items-center">
+        <div className="h-[35vh] w-[30vw] border">
+          <p className="text-xl text-white font-serif mt-5 p-3 text-center font-semibold">
+            Get the Best Fitness Routine and Diet plan for all your health goals
+          </p>
+          <div className="flex w-full justify-center mt-5">
+            <NavLink
+              to="signin"
+              className="p-1.5 bg-[#75dc14] font-semibold text-white rounded-md m-3"
+            >
+              Sign in
+            </NavLink>
+            <NavLink
+              to="login"
+              className="p-1.5 bg-[#ff1f0f] font-semibold text-white rounded-md m-3"
+            >
+              Log in
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

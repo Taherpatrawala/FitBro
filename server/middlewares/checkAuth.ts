a3686378df37c8813bd52515a67ff2e2f5b103d1
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-06-22 10:43:21 +0530

    user authentication using middleware

--- server/middlewares/checkAuth.ts
+++ server/middlewares/checkAuth.ts
@@ -1,7 +1,29 @@
 import { Request, Response, NextFunction } from "express";
+import Jwt from "jsonwebtoken";
 
-const CheckAuth = (req: Request, res: Response, next: NextFunction) => {
+const CheckAuth = async (req: Request, res: Response, next: NextFunction) => {
   console.log("I am in middleware");
-  return res.send("Not authenticated");
+  const token = req.header("authorization");
+  if (!token) {
+    return res.status(403).json({
+      errors: [
+        {
+          msg: "User is unauthorized",
+        },
+      ],
+    });
+  }
+  try {
+    const user = (await Jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    )) as { email: string };
+
+    req.user = user.email;
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.send(token);
 };
 export default CheckAuth;

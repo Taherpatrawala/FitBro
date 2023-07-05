f80bc85113d158a2894a2d5c41e6ef6594af01b8
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-07-05 21:44:25 +0530

    improved some css of the hero

--- client/src/components/body/body.tsx
+++ client/src/components/body/body.tsx
@@ -10,14 +10,14 @@ const Body = () => {
       }}
     >
       <div className="h-full w-full flex justify-center items-center">
-        <div className="h-[35vh] w-[30vw] border">
-          <p className="text-xl text-white font-serif mt-5 p-3 text-center font-semibold">
+        <div className="h-[35vh] esm:w-[55vw] md:w-[30vw] border">
+          <p className="text-xl md:text-2xl lg:text-3xl text-white font-serif mt-5 p-3 text-center font-semibold">
             Get the Best Fitness Routine and Diet plan for all your health goals
           </p>
           <div className="flex w-full justify-center mt-5">
             <NavLink
               to="signin"
-              className="p-1.5 bg-[#75dc14] font-semibold text-white rounded-md m-3"
+              className="p-1.5 bg-[#75dc14] font-semibold text-white rounded-md m-3 esm:text-sm md:text-base"
             >
               Sign in
             </NavLink>

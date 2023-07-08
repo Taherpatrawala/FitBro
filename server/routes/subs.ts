1e0581e71cee5f30fa15f900e7dd855fc801d8a1
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-07-08 19:40:06 +0530

    Returning stripe id from the db

--- server/routes/subs.ts
+++ server/routes/subs.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { body, validationResult } from "express-validator";
 import { stripe } from "../utils/stripe";
 import CheckAuth from "../middlewares/checkAuth";
+import User from "../models/user";
 
 const subsRouter = express.Router();
 
@@ -13,6 +14,7 @@ subsRouter.get("/prices", CheckAuth, async (req, res, next) => {
 });
 
 subsRouter.post("/session", CheckAuth, async (req, res, next) => {
+  const user = await User.findOne({ email: req.user });
   const session = await stripe.checkout.sessions.create({
     mode: "subscription",
     payment_method_types: ["card"],
@@ -24,7 +26,9 @@ subsRouter.post("/session", CheckAuth, async (req, res, next) => {
     ],
     success_url: "http://localhost:5173/articles",
     cancel_url: "http://localhost:5173/articles-plans",
+    customer: user?.customerStripeId,
   });
+  return res.json(session);
 });
 
 export default subsRouter;

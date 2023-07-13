0ef0926f79cb89f6c9cb06cbec8ec589f16859c9
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-07-13 20:08:37 +0530

    oauth update

--- server/models/user.ts
+++ server/models/user.ts
@@ -5,5 +5,6 @@ const userSchema = new Schema({
   email: { type: String, trim: true, required: true, unique: true },
   password: { type: String, required: true, min: 6 },
   customerStripeId: { type: String, required: true },
+  googleId: { type: String },
 });
 export default mongoose.model("User", userSchema);

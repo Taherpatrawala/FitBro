495ad0f32a12c608003494492c10e46b01685cd1
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-07-13 20:07:28 +0530

    oauth update

--- server/routes/auth/google.ts
+++ server/routes/auth/google.ts
@@ -1,16 +1,17 @@
 import passport from "passport";
+import User from "../../models/user";
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://www.example.com/auth/google/callback",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5173/google/callback",
     },
-    function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      User.find({ googleId: profile.id }, function (err, user) {
         return cb(err, user);
       });
     }
